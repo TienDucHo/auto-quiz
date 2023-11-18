9f5be491a81aa26feac5f0eb8bea164d82a15e33
Ho Tien Duc <hotienduc05012002@gmail.com>
2023-11-18 12:27:29 -0700

    Configged Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      white: "#CBD8DF",
+      black: "#141E26",
+      primary: "#1E5067",
+      secondary: "#9AC5D3",
+      accent: "#159AB7",
+      accent1: "#F77474",
+      accent2: "#3DB842",
+      skin: "#E4B3BA",
+    },
+    fontFamily: {
+      sans: ["Nunito", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
